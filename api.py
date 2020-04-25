32eba84f1b879cc745ba6a9f5e57d17adfdec989
Alister Burt <alisterburt@gmail.com>
2020-04-25 14:46:09 +0200

    .

--- api.py
+++ api.py
@@ -12,29 +12,12 @@ app.config['DEBUG'] = True
 
 image = preprocess('./backprojection-simulator/src/assets/rabbit.png')
 
+
 def jsonify_numpy(numpy_array):
     array_list = numpy_array.tolist()
     json = jsonify(array_list)
     return json
 
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
 
 @app.route('/', methods=['GET'])
 def home():
