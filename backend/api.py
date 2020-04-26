5d67aa264f352b78759c56d6098c670327262f21
Alister Burt <alisterburt@gmail.com>
2020-04-26 18:12:51 +0200

    print

--- backend/api.py
+++ backend/api.py
@@ -59,6 +59,8 @@ def api_rotate_image():
     image_rotated = rotate(image, theta)
     filename = resource_path + '/image_rotated.png'
     imsave(filename, image_rotated)
+    print('hoonza')
+    print('boonza')
     return jsonify(filename)
 
 
