af8ae8d3e828524b59091c0cf6357b4088bd0d08
Alister Burt <alisterburt@gmail.com>
2020-04-26 16:47:43 +0200

    update

--- backend/api.py
+++ backend/api.py
@@ -20,35 +20,12 @@ resource_path = os.path.join(app.root_path, 'static')
 image = preprocess(resource_path + '/rabbit.png')
 
 
-
 def jsonify_numpy(numpy_array):
     array_list = numpy_array.tolist()
     json = jsonify(array_list)
     return json
 
 
-<<<<<<< HEAD:backend/api.py
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
-=======
->>>>>>> master:api.py
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Backprojection gimpulator</h1><p>This site is a prototype API for the simulation of backprojection of images</p>"
@@ -85,4 +62,5 @@ def api_rotate_image():
     return jsonify(filename)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
